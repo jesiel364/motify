efc54811b1a8d6ce1d35f5939e7acbc09d69ab93
jesiel364 <jesiel364@gmail.com>
2022-04-23 15:48:55 -0400

    layout Inicial

--- main.py
+++ main.py
@@ -2,33 +2,35 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.utils import platform
 from kivy.core.window import Window
-from kivymd.uix.screen import MDScreentext
-from kivy import ScreenManager
+from kivymd.uix.screen import MDScreen
+from kivy.uix.screenmanager import ScreenManager
 from kivy.properties import StringProperty
+from kivymd.uix.card import MDCard
+
+
+kv = Builder.load_file('main.kv')
 
 if platform != 'android':
     Window.size= 300, 500
 
-class Inicio(MDScreen):
-    def __init__(self, **kw):
-        super().__init__(**kw)
+class CardFrase(MDCard):
+    frase = StringProperty('')
 
-class SM(ScreenManagtexter):
+class Inicio(MDScreen):
+    pass
+
+class SM(ScreenManager):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         self.add_widget(Inicio(name='inicio'))
-def __init__(self, **kw):
-        super().__init__(**kw)
+
 class Motify(MDApp):
     def build(self):
-        self.theme_cls.theme_style= 'Light'
-        self.theme_cls.primary_color = 'Orange'
+        # self.theme_cls.theme_style= 'Light'
+        # self.theme_cls.primary_color = 'Orange'
 
         return SM()
 
 if __name__ == '__main__':
     Motify().run()
-
-def __init__(self, **kwargs):
-        super().__init__(**kwargs)
